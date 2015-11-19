d8b90f25f8fa78585bc36fd680995f7ac80bceed
Tannin <sherb@gmx.net>
2015-11-19 21:35:15 +0100

    initial work

---
+++ src/inibakery.cpp
@@ -0,0 +1,89 @@
+#include "inibakery.h"
+#include <igameinfo.h>
+#include <iplugingame.h>
+#include <utility.h>
+#include <QFile>
+
+
+using namespace MOBase;
+
+
+bool IniBakery::init(MOBase::IOrganizer *moInfo)
+{
+  m_MOInfo = moInfo;
+
+  m_MOInfo->onAboutToRun([] (const QString&) -> bool {
+    // bake the ini on start
+
+    QString profileIni = profileIniPath();
+    if (!QFile::exists(profileIni)) {
+      shellCopy(gameIniPath(), profileIni);
+    }
+
+    WritePrivateProfileStringW(L"Archive", L"sResourceDataDirsFinal",
+                               L"STRINGS\, INTERFACE\, TEXTURES\, MESHES\",
+                               profileIni.toStdWString().c_str());
+  });
+
+  return true;
+}
+
+QString IniBakery::name() const
+{
+  return "Ini Bakery";
+}
+
+QString IniBakery::author() const
+{
+  return "Tannin";
+}
+
+QString IniBakery::description() const
+{
+  return tr("Manages game ini files");
+}
+
+MOBase::VersionInfo IniBakery::version() const
+{
+  return VersionInfo(0, 0, 1, VersionInfo::RELEASE_ALPHA);
+}
+
+bool IniBakery::isActive() const
+{
+  return true;
+}
+
+QList<PluginSetting> IniBakery::settings() const
+{
+  return QList<PluginSetting>();
+}
+
+QString IniBakery::iniFileName() const
+{
+  switch (m_MOInfo->gameInfo().type()) {
+    case IGameInfo::TYPE_OBLIVION:  return "oblivion.ini";
+    case IGameInfo::TYPE_FALLOUT3:  return "fallout.ini";
+    case IGameInfo::TYPE_FALLOUT4:  return "fallout4.ini";
+    case IGameInfo::TYPE_FALLOUTNV: return "fallout.ini";
+    case IGameInfo::TYPE_SKYRIM:    return "skyrim.ini";
+    default: throw std::runtime_error("unsupported game");
+  }
+}
+
+QString IniBakery::profileIniPath() const
+{
+  return m_MOInfo->profilePath() + "/" + iniFileName();
+}
+
+QString IniBakery::gameIniPath() const
+{
+  IPluginGame *game = qApp->property("managed_game").value<IPluginGame*>();
+  return game->documentsDirectory().absoluteFilePath(fileName);
+}
+
+MappingType IniBakery::mappings() const
+{
+
+  return { { profileIniPath(), gameIniPath(), false } };
+}
+
---
+++ src/inibakery.h
@@ -0,0 +1,61 @@
+/*
+Mod Organizer ini bakery plugin
+
+Copyright (C) 2015 Sebastian Herbord. All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#pragma once
+
+
+#include <iplugin.h>
+#include <ipluginfilemapper.h>
+#include <QString>
+#include <QObject>
+#include <QList>
+
+
+/**
+ * @brief The IniBakery class
+ */
+class IniBakery : public MOBase::IPlugin, public MOBase::IPluginFileMapper {
+  Q_OBJECT
+  Q_INTERFACES(MOBase::IPlugin MOBase::IPluginFileMapper)
+  Q_PLUGIN_METADATA(IID "org.tannin.IniBakey" FILE "inibakery.json")
+
+public:
+  virtual bool init(MOBase::IOrganizer *moInfo);
+  virtual QString name() const;
+  virtual QString author() const;
+  virtual QString description() const;
+  virtual MOBase::VersionInfo version() const;
+  virtual bool isActive() const;
+  virtual QList<MOBase::PluginSetting> settings() const;
+
+  virtual MappingType mappings() const;
+
+private:
+
+  QString iniFileName() const;
+  QString profileIniPath() const;
+  QString gameIniPath() const;
+
+private:
+
+  MOBase::IOrganizer *m_MOInfo;
+
+};
+
