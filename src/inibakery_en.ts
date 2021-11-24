463c4296363b1abb58b11728c87203fc8f564b36
Chris Bessent <lost.dragonist@gmail.com>
2021-11-23 20:59:42 -0700

    Update translations

---
+++ src/inibakery_en.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>IniBakery</name>
+    <message>
+        <location filename="inibakery.cpp" line="31"/>
+        <source>INI Bakery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inibakery.cpp" line="41"/>
+        <source>Manages game ini files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
